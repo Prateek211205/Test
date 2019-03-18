93b09c5f455473e003f27432b5c900bc903c8244
Prateek <prateek_c@trigent.com>
2019-03-18 16:11:10 +0530

    First commit

---
+++ force-app/main/default/lwc/webComponentLifeCycle/webComponentLifeCycle.js
@@ -0,0 +1,20 @@
+import { LightningElement } from 'lwc';
+
+export default class WebComponentLifeCycle extends LightningElement {
+    constructor(){
+        super();
+        console.log(this);
+    }
+    connectedCallback(){
+
+        console.log('----------connectedCallback--------');
+    }
+    disconnectedCallback() {
+        console.log('----------disconnectedCallback--------');
+      
+    }
+    renderedCallback(){
+        console.info('----------renderedCallback--------');
+
+    }
+}
